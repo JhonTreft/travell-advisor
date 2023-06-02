60a8103ee2b0bdf58f22b7646ad26d3569a551a7
JhonTreft <jonz37133@gmail.com>
2023-06-02 12:43:24 -0500

    Uploads files

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Autocomplete } from "@react-google-maps/api";
+import { AppBar, Toolbar, Typography, InputBase, Box } from "@material-ui/core";
+
+
+
+const Header = () => {
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                
+            </Toolbar>
+        </AppBar>
+        )
+}
+
+export default Header;
